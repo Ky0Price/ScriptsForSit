4ff0ff23f8d29a2b24188c2513bbe5ca981a2e08
PPPPPrice <2768217281@qq.com>
2021-10-06 09:46:10 +0800

    已更新
    删除了做测试时候写的一些语句
    补充了一点注释
    把输出结果更改为符合ASE输入的模式

--- FormationEnCalc.py
+++ FormationEnCalc.py
@@ -1,6 +1,6 @@
 # 需要和read_ConvexHull中（以下简称rC）产生的数据结构联动
 def FormationEnCalc(idlist,moleEn,system,atomEn):
-    # idlist对应rC中getID()返回的结构ID列表
+    # idlist对应rC中getID()返回的结构ID列表,没发现有什么用，但是删掉会出问题，就给他留着吧
     # moleEn指分子总焓，对应的是rC中getEnthalpies()返回的总焓列表
     # system指体系的组成向量，对应rC中getTotalEnt()返回的组成向量列表
     # atomEn指的是组成分子前每个原子的基态能量，需要自己手动写个字典传入，比如{Li:-2,C:-9}
@@ -10,7 +10,6 @@ def FormationEnCalc(idlist,moleEn,system,atomEn):
         # 计算体系中原子种类数和各原子种类对应的原子数目
         FormationEn = en
         complist = comp.split('_') # 按元素切割一下
-        #testlist.append(complist)
         totalAtomNumber = 0
         composition = ''  # 用于生成方便输入ASE的组成格式
         for ele in complist:
@@ -23,7 +22,6 @@ def FormationEnCalc(idlist,moleEn,system,atomEn):
                     number += i
             totalAtomNumber += eval(number)
             composition += element+number
-            FormationEn -= atomEn[element]*eval(number)  #减去对应原子的基态能量
-        FEdict[composition]=FormationEn/totalAtomNumber
+            FormationEn -= atomEn[element]*eval(number)  # 减去对应原子的基态能量
+        FEdict[composition]=FormationEn   # ASE要求输入的是分子总生成焓，如果想要得到分子中每原子的平均生成焓，请把这一项改为“FEdict[composition]=FormationEn/totalAtomNumber”
     return FEdict
-
