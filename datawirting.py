b4d1dfd5946f6124edcf466300620e9488390f89
PPPPPrice <2768217281@qq.com>
2021-10-19 12:22:32 +0800

    version1.1
    can write spacegroup data to the csv file

--- datawirting.py
+++ datawirting.py
@@ -1,6 +1,6 @@
-def datawirting_exe(datapath,idlist=None,compAndFElist=None,pressure=0,spacegrouplist=None,Enthalpylist=None):
+def datawirting_exe(datapath,comp_FE_id_list=[],pressure=0,spacegrouplist=[],Enthalpylist=[]):
     # 整理数据用
-    # datapath写入你希望保存文件的目录
+    # datapath写入你希望保存文件的位置
     # idlist传入结构的id，如EA123或者mp-1234
     # compAndFEdict传入最后的ref（refs，refs），他包含了结构和对应的分子形成焓
     # spacegrouplist是用其他库生成的,暂时先不整
@@ -12,13 +12,22 @@ def datawirting_exe(datapath,idlist=None,compAndFElist=None,pressure=0,spacegrou
         # 标题行
         head = ['id',
                 'compsition',
+                'spacegroup',
                 'pressure(GPa)',
                 'enthalpy    (ev/atom)',
                 'formation enthalpy    (ev/atom)']
         writer.writerow(head)
-        for id,compAndFE,enthalpy in zip(idlist,compAndFElist,Enthalpylist):
-            comp = compAndFE[0]
-            FE = compAndFE[1]
-            pressure = pressure
-            row = [id]+[comp]+[pressure]+[enthalpy]+[FE]
+        for comp_FE_id,enthalpy in zip(comp_FE_id_list,Enthalpylist):
+            comp = comp_FE_id['composition']
+            FE = comp_FE_id['FE']
+            id = comp_FE_id['id']
+            #print(id)
+            pressure = '0'
+            sg = 0
+            for sgdict in spacegrouplist:
+                if sgdict['id'] == id:
+                    #print(sgdict['number'])
+                    sg += sgdict['number']
+            #print(sg)
+            row = [id]+[comp]+[sg]+[pressure]+[enthalpy]+[FE]
             writer.writerow(row)
